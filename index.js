4bada2ef98e3226cd66e746b37556905cf668143
Ananthakrishnan <ananthusijil@gmail.com>
2024-06-27 23:12:41 +0530

    Add search route

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import express from 'express';
 import axios from 'axios';
 const app = express();
 const port = 3000;
-app.use(express.static('public'))
+app.use(express.static('public'));
 let dropdown;
 app.get('/',async(req,res)=>{
     try{
         const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php");
         dropdown = response.data.categories;
         res.render('index.ejs',{category:dropdown});
-
     }catch(error){
         console.log(error.response);
         res.status(500).send("An error occured");
@@ -44,6 +43,18 @@ app.get('/meal/:id',async(req,res)=>{
     }
     res.render('meals.ejs',{category:dropdown,meal:mealDetails,ingredients:ingredientsWithMeasure});
 
+})
+
+app.get('/search',async(req,res)=>{
+    let id = req.query.search
+    const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`);
+    if(response.data.meals===null){
+      res.send('<h1>Sorry, no such dish.</h1>'); 
+    }else if(response.data.meals.length===1){
+      res.redirect(`/meal/${response.data.meals[0].idMeal}`);
+    }else{
+      res.redirect(`/category/${id}`);
+    }
 })
 
 app.listen(port,()=>{
